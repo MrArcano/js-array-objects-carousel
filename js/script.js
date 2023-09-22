83da3cdad8a3b1a29386add504830c8541be55e1
MrArcano <salvatore-dario@hotmail.it>
2023-09-22 14:45:50 +0200

    [edit]: use forEach

--- js/script.js
+++ js/script.js
@@ -6,21 +6,45 @@ const thumbnailsImg = document.querySelector(".carousel-thumb");
 const btnUp = document.querySelector(".btn-up") 
 const btnDown = document.querySelector(".btn-down")
 
-// Array di img
-const imgArray = [
-  "img/01.webp",
-  "img/02.webp",
-  "img/03.webp",
-  "img/04.webp",
-  "img/05.webp",
+// Array di oggetti
+const images = [
+  {
+      image: 'img/01.webp',
+      title: 'Marvel\'s Spiderman Miles Morale',
+      text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+  }, {
+      image: 'img/02.webp',
+      title: 'Ratchet & Clank: Rift Apart',
+      text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+  }, {
+      image: 'img/03.webp',
+      title: 'Fortnite',
+      text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+  }, {
+      image: 'img/04.webp',
+      title: 'Stray',
+      text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+  }, {
+      image: 'img/05.webp',
+      title: "Marvel's Avengers",
+      text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+  }
 ];
 // -----------------------------------------------
 //  CARICO il div "carousel-img"
 // -----------------------------------------------
-for(let i=0; i<imgArray.length; i++){
-  carouselImg.innerHTML += `<img src="${imgArray[i]}" class="hide img-car">`
-  thumbnailsImg.innerHTML += `<img src="${imgArray[i]}" class="thumb-car">`
-}
+images.forEach((element)=>{
+  carouselImg.innerHTML += `
+  <div class="hide img-car">
+    <img src="${element.image}">
+    <div class="carousel-text">
+      <h3>${element.title}</h3>
+      <p>${element.text}</p>
+    </div>
+  </div>
+  `;
+  thumbnailsImg.innerHTML += `<img src="${element.image}" class="thumb-car">`;
+})
 // -----------------------------------------------
 
 // Indice dell'immagine visibile nel carousel
